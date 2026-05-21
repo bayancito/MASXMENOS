4447314b4763975a03eca905c048275f8323f14b
bayancito <ruiz0brayan586@gmail.com>
2026-05-21 18:15:42 -0400

    feat: registrar perfiles en panel administrativo

--- apps/usuarios/admin.py
+++ apps/usuarios/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Perfil
 
-# Register your models here.
+
+@admin.register(Perfil)
+class PerfilAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "rol",
+        "telefono",
+        "fecha_registro",
+    )
+
+    list_filter = (
+        "rol",
+    )
+
+    search_fields = (
+        "user__username",
+        "user__email",
+    )
